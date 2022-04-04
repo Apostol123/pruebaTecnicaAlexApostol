53076850c24eefaf39793940683f57871c1aaeb1
Alex_Apostol <apostol516@gmail.com>
2022-04-04 21:21:37 +0200

    initial commit

---
+++ PruebaTecnica/PruebaTecnica/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PruebaTecnica
+//
+//  Created by Alex Apostol on 4/4/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
